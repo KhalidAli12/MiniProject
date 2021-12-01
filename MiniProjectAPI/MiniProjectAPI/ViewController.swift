8cb84e44a8c9094c908243f476ca55ad8862b709
Khalid Alshammeri <khalid.Ali.med.s@tuwaiq.edu.sa>
2021-12-01 17:03:23 +0300

    start

---
+++ MiniProjectAPI/MiniProjectAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniProjectAPI
+//
+//  Created by khalid ali on 26/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
