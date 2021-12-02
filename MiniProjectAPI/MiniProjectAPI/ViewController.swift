68fcf51efd37fa693d5baa2150bf5cc77fe4f6ec
Khalid Alshammeri <khalid.Ali.med.s@tuwaiq.edu.sa>
2021-12-02 12:23:53 +0300

    changing to Codable and get the text

--- MiniProjectAPI/MiniProjectAPI/ViewController.swift
+++ MiniProjectAPI/MiniProjectAPI/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var textview: UITextView!
     @IBOutlet weak var textFieldText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,45 +25,38 @@ class ViewController: UIViewController {
             "x-rapidapi-key": "f85098bb3amsh3e6c09680f045ddp12a040jsn41075131d397"
         ]
         
-        let request = NSMutableURLRequest(url: NSURL(string: "https://numbersapi.p.rapidapi.com/1492/year?fragment=true&json=true")! as URL,
-                                          cachePolicy: .useProtocolCachePolicy,
-                                          timeoutInterval: 10.0)
+        var request = URLRequest(url: URL(string: "https://numbersapi.p.rapidapi.com/1492/year?fragment=true&json=true")!)
+
         request.httpMethod = "GET"
         request.allHTTPHeaderFields = headers
         
         let session = URLSession.shared
-        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+        let dataTask = session.dataTask(with: request, completionHandler: { (data, response, error) -> Void in
             if (error != nil) {
                 print(error)
             } else {
-                let httpResponse = response as? HTTPURLResponse
-                        print(httpResponse)
+//                let httpResponse = response as? HTTPURLResponse
+//                        print(httpResponse)
                 //  read json in data
+            }
                 do {
-                    let resultJson = try JSONSerialization.jsonObject(with: data!, options: []) as? [String:Any]
-//                    print("Result",resultJson!)
-                    if let alb = resultJson!["text"] {
-                        print(alb)
-                        
-                        DispatchQueue.main.sync(execute: {
-                            self.textFieldText.text = alb as? String
-                        })
+                    let result = try JSONDecoder().decode(GetAPI.self, from: data!)
+
+                    DispatchQueue.main.async {
+                        print(result.text)
+                        self.textFieldText.text = result.text
                     }
-//                    if let alb2 = resultJson!["type"] {
-//                        print(alb2)
-//                    }
-//                    if let alb3 = resultJson!["number"] {
-//                        print(alb3)
-//                    }
                 } catch {
                     print("Error -> \(error)")
                 }
-            }
         })
         dataTask.resume()
-        
     }
-    
+}
+
+class GetAPI : Codable {
+        var year : Int?
+        var text : String?
 }
 
 
