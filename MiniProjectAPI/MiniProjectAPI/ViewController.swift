a20a4bdef05d7ac84cf26b401681c2e92f72254e
KhalidAli12 <91524742+KhalidAli12@users.noreply.github.com>
2021-12-02 16:55:38 +0300

    Text api (#1)
    * API , func to defining
    textField to give the text from API
    and button
    
    * changing to Codable and get the text
    
    * add found from API to save bool

--- MiniProjectAPI/MiniProjectAPI/ViewController.swift
+++ MiniProjectAPI/MiniProjectAPI/ViewController.swift
@@ -8,12 +8,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textview: UITextView!
+    @IBOutlet weak var textFieldText: UITextField!
+    @IBOutlet weak var textFieldFound: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        getInfo()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func buttontest(_ sender: UIButton) {
+        getInfo()
+    }
+    func getInfo() {
+        let headers = [
+            "x-rapidapi-host": "numbersapi.p.rapidapi.com",
+            "x-rapidapi-key": "f85098bb3amsh3e6c09680f045ddp12a040jsn41075131d397"
+        ]
+        
+        var request = URLRequest(url: URL(string: "https://numbersapi.p.rapidapi.com/1492/year?fragment=true&json=true")!)
+        
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+        
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request, completionHandler: { (data, response, error) -> Void in
+            if (error != nil) {
+                print(error as Any)
+            } else {
+                //                let httpResponse = response as? HTTPURLResponse
+                //                        print(httpResponse)
+                //  read json in data
+            }
+            do {
+                let result = try JSONDecoder().decode(GetAPI.self, from: data!)
+                DispatchQueue.main.async {
+                    print(result.text!)
+                    self.textview.text = result.text
+                    print(result.number!)
+                    self.textFieldText.text = result.number?.description
+                    print(result.found!)
+                    self.textFieldFound.text = result.found?.description
+                }
+            } catch {
+                print("Error -> \(error)")
+            }
+        })
+        dataTask.resume()
+    }
 }
+
+struct GetAPI : Codable {
+    var number : Int?
+    var text : String?
+    var found : Bool?
+}
+
 
