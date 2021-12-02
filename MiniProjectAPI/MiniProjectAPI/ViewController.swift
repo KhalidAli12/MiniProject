a05e1edb43580685ec1bc4e374e033bd25343be1
Khalid Alshammeri <khalid.Ali.med.s@tuwaiq.edu.sa>
2021-12-02 16:54:54 +0300

    add found from API to save bool

--- MiniProjectAPI/MiniProjectAPI/ViewController.swift
+++ MiniProjectAPI/MiniProjectAPI/ViewController.swift
@@ -8,9 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     @IBOutlet weak var textview: UITextView!
     @IBOutlet weak var textFieldText: UITextField!
+    @IBOutlet weak var textFieldFound: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         getInfo()
@@ -26,37 +26,41 @@ class ViewController: UIViewController {
         ]
         
         var request = URLRequest(url: URL(string: "https://numbersapi.p.rapidapi.com/1492/year?fragment=true&json=true")!)
-
+        
         request.httpMethod = "GET"
         request.allHTTPHeaderFields = headers
         
         let session = URLSession.shared
         let dataTask = session.dataTask(with: request, completionHandler: { (data, response, error) -> Void in
             if (error != nil) {
-                print(error)
+                print(error as Any)
             } else {
-//                let httpResponse = response as? HTTPURLResponse
-//                        print(httpResponse)
+                //                let httpResponse = response as? HTTPURLResponse
+                //                        print(httpResponse)
                 //  read json in data
             }
-                do {
-                    let result = try JSONDecoder().decode(GetAPI.self, from: data!)
-
-                    DispatchQueue.main.async {
-                        print(result.text)
-                        self.textFieldText.text = result.text
-                    }
-                } catch {
-                    print("Error -> \(error)")
+            do {
+                let result = try JSONDecoder().decode(GetAPI.self, from: data!)
+                DispatchQueue.main.async {
+                    print(result.text!)
+                    self.textview.text = result.text
+                    print(result.number!)
+                    self.textFieldText.text = result.number?.description
+                    print(result.found!)
+                    self.textFieldFound.text = result.found?.description
                 }
+            } catch {
+                print("Error -> \(error)")
+            }
         })
         dataTask.resume()
     }
 }
 
-class GetAPI : Codable {
-        var year : Int?
-        var text : String?
+struct GetAPI : Codable {
+    var number : Int?
+    var text : String?
+    var found : Bool?
 }
 
 
